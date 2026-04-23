72bd6e3591c75f66a7a7da1ea44680550dcd1a24
IGAL SALOMON DUJOVNE <49836363@ort>
2026-04-23 08:57:18 -0300

    terminado

--- Models/Grupo.cs
+++ Models/Grupo.cs
@@ -7,20 +7,22 @@ public class Grupo
 
     public Grupo ()
     {
-        
+
+        Integrantes= new Dictionary<int, Integrante>();
         CargaDatosManual();
         
     }
     public void CargaDatosManual()
     {
-        List<string> FamiliaresTomu = new List <string> {"La maquina fernando (papa), Betiana(mama), Agustin(Hermano)"};
-        List<string> FamiliaresPigu = new List <string> {"La maquina Mariano (papa), Yael(mama), Eitan(Hermano), Carmit(hermana)"};
+        List<string> FamiliaresTomu = new List <string> {"La maquina fernando (papa)", "Betiana(mama)", "Agustin(Hermano)"};
+        List<string> FamiliaresPigu = new List <string> {"La maquina Mariano (papa)", "Yael(mama)", "Eitan(Hermano)", "Carmit(hermana)"};
         List<string> InteresesTomu = new List<string> {"Padel", "Peliculas", "Juegos"};
         List<string> InteresesPigu = new List<string> {"Futbol", "Series", "Ropa"};
-        Integrante i2= new Integrante ("Tomas", 50324501, "tomu.jfif", new DateTime(2010, 03, 06), FamiliaresTomu, InteresesTomu);
-        Integrante i1= new Integrante("Igal", 49836363,"pigu.jfif", new DateTime(2009,01,11), FamiliaresPigu, InteresesPigu);
-        Integrantes.Add(49836363, i1);
-        Integrantes.Add(50324501, i2);
+        Integrante i2= new Integrante ("Tomas", 50324501, "/Imagenes/tomu.jpg", new DateTime(2010, 03, 06), FamiliaresTomu, InteresesTomu);
+        Integrante i1= new Integrante("Igal", 49836363,"/Imagenes/pigu.jpg", new DateTime(2009,01,11), FamiliaresPigu, InteresesPigu);
+        
+        Integrantes.Add(i1.GetDNI(), i1);
+        Integrantes.Add(i2.GetDNI(), i2);
     }
     public Dictionary<int, Integrante> DevolverIntegrantes()
     {
--- Models/Integrante.cs
+++ Models/Integrante.cs
@@ -41,4 +41,4 @@ public class Integrante
     {
         return this.Intereses;
     }
-}
+}   
